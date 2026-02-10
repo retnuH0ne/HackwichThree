7df137217f63423fe1cb96d7f4c8d0371623dbd9
Hunter Trujillo <megafan.exe@gmail.com>
2026-02-10 03:18:39 -1000

    Hackwich 3 Completed.

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -12,13 +12,24 @@ class ViewController: UIViewController {
     var secondString = "The background color will turn green"
     var coolString = "Cool"
     
+    var numberOne: Int = 1
+    var numberTwo: Int = 2
+    
+    
     @IBOutlet weak var coolLabel: UILabel!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    
     
    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         coolLabel.text = " "
+        firstLabel.text = " "
+        secondLabel.text = " "
+        
     }
     
     @IBAction func changeColorButton(_ sender: Any)
@@ -35,6 +46,41 @@ class ViewController: UIViewController {
                 coolLabel.text = "This is  \(coolString)"
             }
         
+    }
+    
+    
+    @IBAction func changeTextButton(_ sender: Any)
+    {
+        if firstString == "The background color will turn blue"
+        {
+            firstLabel.text = "The text has been changed."
+            
+        }
+        else
+        {
+            secondLabel.text = "This text should not be changed."
+        }
+        
+    }
+    
+    
+    @IBAction func additionButton(_ sender: Any)
+    {
+        if numberOne >= numberTwo
+        {
+            print("The code in this block will not exceute.")
+            
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.green
+            
+            let sum: Int = numberOne + numberTwo
+            
+            print(sum)
+            
+            
+        }
     }
     
 }
